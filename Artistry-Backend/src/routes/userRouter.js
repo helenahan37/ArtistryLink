ec6616ff71428fe57e6e4ff240c5123320d5e2ab
Helena.Han <hlyhelena37@gmail.com>
2024-04-15 01:45:11 +1000

    Update userRouter.js

--- Artistry-Backend/src/routes/userRouter.js
+++ Artistry-Backend/src/routes/userRouter.js
@@ -15,7 +15,8 @@ const userRoutes = express.Router();
 userRoutes
 	.post('/register', registerUser)
 	.post('/login', loginUser)
-	.get('/profile/:userId', checkLoggedIn, getUserProfile) // Access for logged-in users and only account owner can access the profile
+	.get('/profile', checkLoggedIn, getUserProfile) // Access for logged-in users and only account owner can access the profile
+	.get('/:id/profile', checkLoggedIn, getUserProfileById) // Access for any users to access user profile by id
 	.patch('/settings', checkLoggedIn, avatarsUpload.single('file'), updateUserDetails) //Access for logged-in users and only account owner can access the setting page
 	.delete('/settings/delete', checkLoggedIn, deleteUser); //Access for logged-in users and only account owner can delete the account
 
